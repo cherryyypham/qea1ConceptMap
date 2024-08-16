ddff2bcf036ae145fe972144a8022019fd9b5ef0
Cherry Pham <meokkmun@gmail.com>
2024-08-16 03:02:29 -0400

    Syntax clean up

--- src/utils/nodes-edges.js
+++ src/utils/nodes-edges.js
@@ -4,23 +4,22 @@ export const initialNodes = [];
 export const initialEdges = [];
 
 (async () => {
-    const data = await parseCSV('/csvGen.csv');
+  const data = await parseCSV('/csvGen.csv');
 
-    data.forEach((item, index) => {
-        initialNodes.push({
-            id: item.lgLabel,
-            position: { x: 0, y: index * 100 },
-            data: { label: item.lgLabel }
-        });
-
-        item.conPostReqs.forEach(conPostReq => {
-            initialEdges.push({
-                id: `${item.lgLabel}-${conPostReq}`,
-                source: item.lgLabel,
-                target: conPostReq,
-                animated: true,
-            });
-        });
+  data.forEach((item, index) => {
+    initialNodes.push({
+      id: item.lgLabel,
+      position: { x: 0, y: index * 100 },
+      data: { label: item.lgLabel },
     });
-}
-)();
+
+    item.conPostReqs.forEach((conPostReq) => {
+      initialEdges.push({
+        id: `${item.lgLabel}-${conPostReq}`,
+        source: item.lgLabel,
+        target: conPostReq,
+        animated: true,
+      });
+    });
+  });
+})();
