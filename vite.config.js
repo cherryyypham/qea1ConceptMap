2199c0f2827b9802bca91f279774203fcded882d
Cherry Pham <meokkmun@gmail.com>
2025-02-16 18:55:23 -0500

    Push unfinished changes to clean mac

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   base: '/qea1ConceptMap/',
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   resolve: {
     alias: {
       '@utils': '/src/utils',
     },
     extensions: ['.js', '.jsx', '.ts', '.tsx']
-  }
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // kB
+    // rollupOptions: { // Manudal chunking
+    //   output: {
+    //     manualChunks: {
+    //       vendor: ['react', 'react-dom'],
+    //       lodash: ['lodash'],
+    //       moment: ['moment'],
+    //     },
+    //   },
+    // },
+  },
 });
